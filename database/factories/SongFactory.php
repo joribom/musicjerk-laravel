0aa948dad83e644657c05877418821b727d09c50
Johan Ribom <jribom@Johans-MacBook-Pro.local>
2019-07-11 11:45:00 +0200

    :construction: Add initial song model

---
+++ database/factories/SongFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Song;
+use Faker\Generator as Faker;
+
+$factory->define(Song::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
