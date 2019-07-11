d2aec4495de5b16dcfcd6e539713cdb710980046
Johan Ribom <jribom@Johans-MacBook-Pro.local>
2019-07-11 11:44:11 +0200

    :construction: Add initial model for Album

---
+++ database/factories/AlbumFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Album;
+use Faker\Generator as Faker;
+
+$factory->define(Album::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
