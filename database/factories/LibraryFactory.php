8e91c7c8713f7fc7a5f42673f439869ed26160d6
Johan Ribom <johan.ribom@hotmail.com>
2019-08-19 15:58:50 +0200

    :construction: Try to add bootstrap theme

---
+++ database/factories/LibraryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Library;
+use Faker\Generator as Faker;
+
+$factory->define(Library::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
