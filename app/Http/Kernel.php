045570463ecb9be9acbe8c56068c59343090bf6d
Johan Ribom <johan.ribom@slagkryssaren.com>
2020-10-18 13:52:58 +0200

    :construction: Add Laravel Sanctum dependency

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,6 +38,9 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
             'throttle:60,1',
             'bindings',
         ],
